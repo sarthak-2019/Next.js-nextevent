6b8f8fc81783c1e0db4c3cd0bc326f91f70a42c4
Sarthak Shukla <sarthak2019fractal@gmail.com>
2022-01-20 20:55:01 +0530

    All Events Page

---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FilteredEventsPage = () => {
+  return <div>Sarthak</div>;
+};
+
+export default FilteredEventsPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
-const Events = () => {
-  return <div>sarthak</div>;
-};
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
-export default Events;
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
